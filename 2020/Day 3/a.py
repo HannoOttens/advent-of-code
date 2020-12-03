6c868f6efe88a88123908a04f5fa628b93b5e69b
Hanno <hanno.ottens@hotmail.com>
2020-12-03 09:25:38 +0100

    Day 3

---
+++ 2020/Day 3/a.py
@@ -0,0 +1,13 @@
+with open("a.txt", "r") as f:
+    lines = f.read().splitlines() 
+w = len(lines[0])
+h = len(lines)
+dx = 3
+dy = 1
+px = py = trees = 0
+while py < h:
+    if lines[py][px%w] == '#':
+        trees += 1
+    px += dx
+    py += dy
+print(trees)
---
+++ 2020/Day 3/b.py
@@ -0,0 +1,16 @@
+with open("a.txt", "r") as f:
+    lines = f.read().splitlines() 
+w = len(lines[0])
+h = len(lines)
+total = 1
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+for (dx,dy) in slopes:
+    px = py = trees = 0
+    while py < h:
+        if lines[py][px%w] == '#':
+            trees += 1
+        px += dx
+        py += dy
+    print(f'Slope: {dx,dy}, Trees: {trees}')
+    total *= trees
+print(f'Total: {total}')

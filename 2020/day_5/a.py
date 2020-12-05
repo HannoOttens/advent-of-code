73d8ae975d6858f8c5e3f8b64ae3c7c8309c44ab
Hanno <hanno.ottens@hotmail.com>
2020-12-05 12:50:07 +0100

    Clean up a.py a bit

--- 2020/day_5/a.py
+++ 2020/day_5/a.py
@@ -1,10 +1,7 @@
 with open("a.txt", "r") as f:
     lines = f.read().splitlines()
-n = 1023
-my_seat = n * (n + 1) / 2
 _max = 0
 for seat in lines:
     num = seat.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0')
-    n = int('0b'+num, 2)
-    _max = max(n, _max)
+    _max = max(int('0b'+num, 2), _max)
 print(_max)

49b25bedb868a585c5cfe2898ef9bf00af676d15
Hanno <hanno.ottens@hotmail.com>
2020-12-06 12:42:01 +0100

    Day 6 cleanup

--- 2020/day_6/a.py
+++ 2020/day_6/a.py
@@ -1,19 +1,11 @@
-import string
-
 with open("input.txt", "r") as f:
     file = f.read() 
 
 new_group = '\n\n'
 groups = file.split(new_group)
-letters = string.ascii_lowercase[:26]
 
 total_questions = 0
 for group in groups:
-    persons = group.split('\n')
-
-    validation_map = dict(map(lambda t: (t, False), letters))
-    for person in persons:
-        for l in person:
-            validation_map[l] = True
-    total_questions += sum(validation_map.values())
+    group = group.replace('\n','')
+    total_questions += len(set(group))
 print(total_questions)
--- 2020/day_6/b.py
+++ 2020/day_6/b.py
@@ -1,17 +1,13 @@
-import string
-
 with open("input.txt", "r") as f:
     file = f.read() 
 
 new_group = '\n\n'
 groups = file.split(new_group)
-letters = string.ascii_lowercase[:26]
 
 total_questions = 0
 for group in groups:
     persons = group.split('\n')
-
-    letter_set = set(string.ascii_lowercase[:26])
+    letter_set = set(persons[0])
     for person in persons:
         letter_set = letter_set.intersection(set(person))
     total_questions += len(letter_set)

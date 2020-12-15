f66caa22a6d257dd54c8ec4bef202907926aeea2
Hanno <hanno.ottens@hotmail.com>
2020-12-15 09:46:17 +0100

    Day 15: Nice an slow

---
+++ 2020/day_15/b.py
@@ -0,0 +1,43 @@
+
+from parsers import *
+from collections import defaultdict
+
+# ===================
+# Code
+# ===================
+
+def main():
+    with open("input.txt", "r") as f:
+        rules = f.read()
+
+    # Parse
+    (numbers, rest) = parseNums(rules)
+    if rest != "":
+        raise Exception("Remaining input in parser:\n" + rest)
+    
+    d = defaultdict(lambda: [])
+    for i, n in enumerate(numbers):
+        d[n].append(i)
+
+    n = numbers[-1]
+    for i in range(len(numbers), 30000000):
+        if i % 30000 == 0:
+            print(f'Progress: {i/300000}%')
+        if len(d[n]) <= 1:
+            n2 = 0
+        else:
+            n2 = d[n][-1] - d[n][-2]
+        d[n2].append(i)
+        n = n2
+
+        
+    print(n)
+# ===================
+# Algebra
+# ===================
+
+def parseNums(s):
+    return (parseList(parseChar(','), parseNum))(s)
+
+# Call main
+main()

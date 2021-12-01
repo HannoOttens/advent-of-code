3b036e5cee545fccb6516f1be7dd4a1ff1797f61
Hanno Ottens <hpottens@arkey.systems>
2021-12-01 19:33:22 +0100

    Day 1: Part 2

--- 2021/day_1/b/Program.cs
+++ 2021/day_1/b/Program.cs
@@ -3,12 +3,25 @@
 var lines = File.ReadAllLines("../input.txt");
 
 int incr = 0;
-int wind = 0;
-int wins = new int{ 0,0,0 };
+int iter = 0;
+int currWind = 0;
+var windows = new int[]{ 0,0,0,0 };
 foreach (var l in lines) {
+	if (iter > 3) {
+		int nextWind = (currWind + 1) % 4;
+		if (windows[currWind] < windows[nextWind]) 
+			incr++;
+		windows[currWind] = 0;
+	}
+	
 	int curr = int.Parse(l);
-	if(curr > prev) incr++; 
-	prev = curr;
+	if (iter % 4 != 3) windows[0] += curr; 
+	if (iter % 4 != 0) windows[1] += curr; 
+	if (iter % 4 != 1) windows[2] += curr; 
+	if (iter % 4 != 2) windows[3] += curr; 
+	currWind = (currWind + 1) % 4;
+	
+	iter++;
 }
 
 Console.WriteLine(incr);

0a31ac564299937b394ff52058c6313b267ae9fe
Hanno Ottens <hpottens@arkey.systems>
2021-12-01 17:29:33 +0100

    Day 1: Part 1

---
+++ 2021/day_1/a/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("../input.txt");
+
+int prev = int.MaxValue;
+int incr = 0;
+foreach (var l in lines) {
+	int curr = int.Parse(l);
+	if(curr > prev) incr++; 
+	prev = curr;
+}
+
+Console.WriteLine(incr);
---
+++ 2021/day_1/b/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("../input.txt");
+
+int incr = 0;
+int wind = 0;
+int wins = new int{ 0,0,0 };
+foreach (var l in lines) {
+	int curr = int.Parse(l);
+	if(curr > prev) incr++; 
+	prev = curr;
+}
+
+Console.WriteLine(incr);

b786333b8ba6e2cecde91038fd2e914919ffdf7d
Hanno Ottens <hpottens@arkey.systems>
2021-12-02 07:31:06 +0100

    Day 2: Diving with submarines

---
+++ 2021/day_2/a/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("../input.txt");
+
+int x = 0;
+int z = 0;
+foreach (var l in lines) {
+	var parts = l.Split(' ');
+	int n = int.Parse(parts[1]);
+
+	switch (parts[0]) {
+		case "forward":
+			x += n; break;
+		case "down":
+			z += n; break;
+		case "up":
+			z -= n; break;
+	}
+}
+
+Console.WriteLine(x*z);
---
+++ 2021/day_2/b/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("../input.txt");
+
+int x = 0;
+int a = 0;
+int d = 0;
+foreach (var l in lines) {
+	var parts = l.Split(' ');
+	int n = int.Parse(parts[1]);
+
+	switch (parts[0]) {
+		case "forward":
+			x += n; 
+			d += n * a;
+			break;
+		case "down":
+			a += n; break;
+		case "up":
+			a -= n; break;
+	}
+}
+
+Console.WriteLine(x*d);

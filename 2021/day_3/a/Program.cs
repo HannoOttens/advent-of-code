663c5d00cd99612f39c3ff3216574d57968745ea
Hanno Ottens <hpottens@arkey.systems>
2021-12-03 09:02:53 +0100

    Day 3: Binary into the bin

---
+++ 2021/day_3/a/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("../input.txt");
+
+var zero = new int[lines[0].Length];
+var ones = new int[lines[0].Length];
+var actv = new bool[lines.Length];
+
+foreach (var l in lines) 
+	for(int i = 0; i < ones.Length; i++) 
+		if (l[i] == '0') zero[i]++; else ones[i]++;
+
+int gamma = 0;
+int epsil = 0;
+int b = (1 << ones.Length - 1);
+for(int i = 0; i < ones.Length; i++) {
+	gamma += b * (ones[i] > zero[i] ? 1 : 0);
+	epsil += b * (ones[i] < zero[i] ? 1 : 0);
+	b /= 2;
+}
+
+Console.WriteLine(gamma * epsil);
---
+++ 2021/day_3/b/Program.cs
@@ -0,0 +1,72 @@
+﻿var lines = File.ReadAllLines("../input.txt");
+var actv = new bool[lines.Length];
+
+// Count active bits
+(int zero, int ones) BitCount(int bit) {
+	int ones = 0;
+	int zero = 0;
+	for (int j = 0; j < lines.Length; j++)
+	{
+		if (actv[j])
+			if (lines[j][bit] == '0') ones++; else zero++;
+	}
+	return (ones,zero);
+}
+
+// Filter numbers on bit
+void Filter(int bit, char val) {
+	for (int j = 0; j < lines.Length; j++)
+		if (lines[j][bit] != val) actv[j] = false;
+}
+
+// Find last number
+(bool last, string numm) Find(int bit) {
+	int actC = 0;
+	string actN = "";
+	for (int j = 0; j < lines.Length; j++)
+		if (actv[j]) {
+			actN = lines[j];
+			actC++;
+		}
+	return (actC == 1, actN);
+}
+
+string oxy = "";
+for (int i = 0; i < lines.Length; i++)
+	actv[i] = true;
+for (int bit = 0; bit < lines[0].Length; bit++) {
+	// Vind MCB 
+	(int zero, int ones) = BitCount(bit);
+	Filter(bit, ones >= zero ? '1': '0');
+	
+	// Stop bij 1
+	(bool hasLast, oxy) = Find(bit);
+	if (hasLast) break;
+}
+
+// Copypasta omdat lui
+string co2 = "";
+for (int i = 0; i < lines.Length; i++)
+	actv[i] = true;
+for (int bit = 0; bit < lines[0].Length; bit++) {
+	// Vind MCB 
+	(int zero, int ones) = BitCount(bit);
+	Filter(bit, ones < zero ? '1': '0');
+	
+	// Stop bij 1
+	(bool hasLast, co2) = Find(bit);
+	if (hasLast) 
+		break;
+}
+
+
+int oxyN = 0;
+int co2N = 0;
+int b = (1 << oxy.Length - 1);
+for(int i = 0; i < oxy.Length; i++) {
+	oxyN += b * (oxy[i] == '1' ? 1 : 0);
+	co2N += b * (co2[i] == '1' ? 1 : 0);
+	b /= 2;
+}
+
+Console.WriteLine(oxyN * co2N);

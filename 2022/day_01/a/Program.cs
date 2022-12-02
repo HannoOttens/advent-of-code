445cbbed4298e34c59e00d87dccb6c37f3eb05c8
Hanno Ottens <hpottens@arkey.systems>
2022-12-02 09:46:49 +0100

    Day 1: Calories

---
+++ 2022/day_01/a/Program.cs
@@ -0,0 +1,15 @@
+﻿var lines = File.ReadAllLines("../in.txt");
+
+int maxC = 0;
+int curC = 0;
+foreach (var l in lines) {
+	if (string.IsNullOrEmpty(l)) {
+		maxC = Math.Max(maxC, curC);
+		curC = 0;
+	} else {
+		int curr = int.Parse(l);
+		curC += curr;
+	}
+}
+
+Console.WriteLine(maxC);
---
+++ 2022/day_01/b/Program.cs
@@ -0,0 +1,25 @@
+﻿var lines = File.ReadAllLines("../in.txt");
+
+int[] maxC = new int[3] { 0, 0, 0};
+
+int lowIndx() {
+	if (maxC[0] <= maxC[1] && maxC[0] <= maxC[2])
+		return 0;
+	if (maxC[1] <= maxC[2] && maxC[1] <= maxC[0])
+		return 1;
+	return 2;
+}
+
+int curC = 0;
+foreach (var l in lines) {
+	if (string.IsNullOrEmpty(l)) {
+		int indx = lowIndx();
+		maxC[indx] = Math.Max(maxC[indx], curC);
+		curC = 0;
+	} else {
+		int curr = int.Parse(l);
+		curC += curr;
+	}
+}
+
+Console.WriteLine(maxC.Sum());

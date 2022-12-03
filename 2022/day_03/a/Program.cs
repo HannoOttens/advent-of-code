db64c762e43fd25dacb6362b2c8e16612698b479
Hanno Ottens <hpottens@arkey.systems>
2022-12-03 10:44:14 +0100

    Day 3: Packing a rucksack

---
+++ 2022/day_03/a/Program.cs
@@ -0,0 +1,29 @@
+﻿var lines = File.ReadAllLines("../in.txt");
+
+int charToIndx (char c) {
+	if (c >= 'a') 
+		return (int)c - (int)'a';
+	return (int)c - (int)'A' + 26;
+}
+
+
+int prio = 0;
+ulong cntA;
+ulong cntB;
+foreach (var l in lines) {
+	cntA = 0;
+	cntB = 0;
+
+	int splt = l.Length / 2;
+	var sckA = l.Substring(0, splt);
+	var sckB = l.Substring(splt, splt);
+
+	for (int i = 0; i < splt; i++) {
+		cntA |= (1UL << charToIndx(sckA[i]));
+		cntB |= (1UL << charToIndx(sckB[i]));
+	}
+
+	ulong same = cntA & cntB;
+	prio += (int)Math.Log2(same) + 1;
+}
+Console.WriteLine(prio);
---
+++ 2022/day_03/b/Program.cs
@@ -0,0 +1,25 @@
+﻿var lines = File.ReadAllLines("../in.txt");
+
+int charToIndx (char c) {
+	if (c >= 'a') 
+		return (int)c - (int)'a';
+	return (int)c - (int)'A' + 26;
+}
+
+int prio = 0;
+ulong grup = ulong.MaxValue;
+for (int elfI = 0; elfI < lines.Length; elfI++) {
+	ulong elfB = 0;
+	for (int i = 0; i < lines[elfI].Length; i++) {
+		elfB |= (1UL << charToIndx(lines[elfI][i]));
+	}
+	grup &= elfB;
+
+	if (elfI % 3 == 2) {
+		prio += (int)Math.Log2(grup) + 1;
+		grup = ulong.MaxValue;
+	}
+}
+
+
+Console.WriteLine(prio);

326ee830bb87d251cce1c9355305d089e77f30ff
Hanno Ottens <hpottens@arkey.systems>
2022-12-04 15:07:54 +0100

    Day 4: Pairing pairs

---
+++ 2022/day_04/b/Program.cs
@@ -0,0 +1,14 @@
+﻿var lines = File.ReadAllLines("../in.txt");
+
+bool overlap (int a1, int a2, int b1, int b2) 
+	=> (a1 >= b1 && a1 <= b2) 
+	|| (a2 >= b1 && a2 <= b2)
+	|| (b1 >= a1 && b1 <= a2)
+	|| (b2 >= a1 && b2 <= a2);
+
+int totl = 0;
+foreach (var l in lines) {
+	var pairs = l.Split(',').Select((x) => x.Split('-').Select(int.Parse).ToList()).ToList();
+	totl += (overlap(pairs[0][0], pairs[0][1], pairs[1][0], pairs[1][1]) ? 1 : 0);
+}
+Console.WriteLine(totl);

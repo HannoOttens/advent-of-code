310a695ba8f4b63b3ffb1544c5e7b70c9a99bfbd
Hanno Ottens <hpottens@arkey.systems>
2022-12-05 10:06:16 +0100

    Day 5: Stacks upon stacks

---
+++ 2022/day_05/a/Program.cs
@@ -0,0 +1,27 @@
+﻿var lines = File.ReadAllText("../in.txt");
+var parts = lines.Split("\r\n\r\n").ToList();
+
+// Parse the stacks
+Stack<char>[] stck = new Stack<char>[9] { new(), new(), new(), new(), new(), new(), new(), new(), new() }; 
+foreach(var line in parts[0].Split("\r\n").Take(8).Reverse()) {
+	var blks = line.Chunk(4).ToList(); 
+	for (int i = 0; i < 9; i++)  {
+		if (blks[i][0] == '[') stck[i].Push(blks[i][1]);
+	}
+}
+
+// Run the input
+foreach(var line in parts[1].Split("\r\n")) {
+	Console.WriteLine(line);
+	var cmnd = line.Split(' ').Where((_, indx) => indx % 2 != 0).Select(int.Parse).ToList();
+	int numb = cmnd[0];
+	int from = cmnd[1] - 1;
+	int to   = cmnd[2] - 1;
+	while (numb > 0) {
+		stck[to].Push(stck[from].Pop());
+		numb--;
+	}
+}
+
+for(int i = 0; i < stck.Length; i++)
+	Console.Write(stck[i].Pop());

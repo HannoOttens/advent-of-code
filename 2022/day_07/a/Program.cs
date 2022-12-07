325b97cff9c3da1f40e95a64ce52446d8345cbba
Hanno Ottens <hpottens@arkey.systems>
2022-12-07 09:27:18 +0100

    Day 7: The-rectory

---
+++ 2022/day_07/a/Program.cs
@@ -0,0 +1,59 @@
+﻿string[] cmds = File.ReadAllLines("../in.txt");
+
+
+Node root = new();
+root.Name = "/";
+Node curr = root;
+
+// Build tree
+foreach(string comd in cmds) {
+	var parts = comd.Split(' ');
+	switch (parts[0] + " " + parts[1]) {
+		case "$ cd":
+			curr = parts[2] switch {
+				".." => curr.Up,
+				"/"  => root,
+				_    => curr.Chld.Find((x) => x.Name == parts[2])
+			};
+			break;
+		case "$ ls":
+			break;
+		default:
+			Node n = new();
+			n.Up = curr;
+			n.Name = parts[1];
+			if (int.TryParse(parts[0], out int Size))
+				n.Size = Size;
+			if (!curr.Chld.Any((x) => x.Name == n.Name))
+				curr.Chld.Add(n);
+			break;
+	}
+}
+
+// Traverse tree en set dir sizes
+int dirSizes (Node n) {
+	int size = 0;
+	foreach(Node node in n.Chld) 
+		size += dirSizes(node);
+	n.Size += size;
+	return n.Size;
+}
+dirSizes(root);
+
+// Sum up all directory sizes
+int dir100k (Node n) {
+	int size = 0;
+	foreach(Node node in n.Chld) 
+		size += dir100k(node);
+	if (n.Chld.Count > 0 && n.Size < 100_000)
+		size += n.Size;
+	return size;
+}
+Console.WriteLine(dir100k(root));
+
+class Node {
+	public string Name;
+	public int Size = 0;
+	public Node Up;
+	public List<Node> Chld = new();
+}
---
+++ 2022/day_07/b/Program.cs
@@ -0,0 +1,62 @@
+﻿string[] cmds = File.ReadAllLines("../in.txt");
+
+Node root = new();
+root.Name = "/";
+Node curr = root;
+
+// Build tree
+foreach(string comd in cmds) {
+	var parts = comd.Split(' ');
+	switch (parts[0] + " " + parts[1]) {
+		case "$ cd":
+			curr = parts[2] switch {
+				".." => curr.Up,
+				"/"  => root,
+				_    => curr.Chld.Find((x) => x.Name == parts[2])
+			};
+			break;
+		case "$ ls":
+			break;
+		default:
+			Node n = new();
+			n.Up = curr;
+			n.Name = parts[1];
+			if (int.TryParse(parts[0], out int Size))
+				n.Size = Size;
+			curr.Chld.Add(n);
+			break;
+	}
+}
+
+// Traverse tree en set dir sizes
+int dirSizes (Node n) {
+	int size = 0;
+	foreach(Node node in n.Chld) 
+		size += dirSizes(node);
+	n.Size += size;
+	return n.Size;
+}
+dirSizes(root);
+
+// Find smallest directory that frees up enough space
+int free = 70_000_000 - root.Size;
+int need = Math.Abs(free - 30_000_000);
+Console.WriteLine(need);
+
+int smallestDir = int.MaxValue;
+void dirDelete (Node n) {
+	if (n.Chld.Count > 0 && n.Size > need)
+		smallestDir = Math.Min(smallestDir, n.Size);
+	foreach(Node node in n.Chld) 
+		dirDelete(node);
+}
+
+dirDelete(root);
+Console.WriteLine(smallestDir);
+
+class Node {
+	public string Name = "";
+	public int Size = 0;
+	public Node? Up;
+	public List<Node> Chld = new();
+}

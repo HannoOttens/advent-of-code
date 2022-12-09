fd26f77e32b0526749508301694bf997aff64246
Hanno Ottens <hpottens@arkey.systems>
2022-12-09 11:51:46 +0100

    Day 9: All tied up

---
+++ 2022/day_09/a/Program.cs
@@ -0,0 +1,33 @@
+﻿(char, int)[] cmnds = File.ReadAllLines("../in.txt").Select(t => (t[0], int.Parse(t[2..]))).ToArray();
+
+HashSet<Posi> beenThereDoneThat = new();
+Posi head = new(0,0);
+Posi tail = new(0,0);
+beenThereDoneThat.Add(tail);
+foreach ((char dir, int length) in cmnds) {
+	Posi step = dir switch {
+		'U' => new( 0,  1),
+		'D' => new( 0, -1),
+		'R' => new( 1,  0),
+		'L' => new(-1,  0),
+	};
+
+	for(int i = 0; i < length; i++)
+	{
+		head += step;
+		if ((head - tail).Dist() > 1) {
+			tail = head - step; // Tail always goes where the head came from
+			beenThereDoneThat.Add(tail);
+		}
+	}
+}
+Console.WriteLine(beenThereDoneThat.Count);
+
+public record Posi(int PosX, int PosY)
+{
+	public static Posi operator +(Posi a, Posi b)
+		=> new(a.PosX + b.PosX, a.PosY + b.PosY);
+	public static Posi operator -(Posi a, Posi b)
+		=> new(a.PosX - b.PosX, a.PosY - b.PosY);
+	public int Dist() => Math.Max(Math.Abs(PosX), Math.Abs(PosY));
+}
---
+++ 2022/day_09/b/Program.cs
@@ -0,0 +1,33 @@
+﻿(char, int)[] cmnds = File.ReadAllLines("../in.txt").Select(t => (t[0], int.Parse(t[2..]))).ToArray();
+
+HashSet<Posi> beenThereDoneThat = new();
+Posi[] rope = new Posi[10] { new(), new(), new(), new(), new(), new(), new(), new(), new(), new()};
+beenThereDoneThat.Add(rope[9]);
+foreach ((char dir, int length) in cmnds) {
+	for(int i = 0; i < length; i++)
+	{
+		rope[0] += dir switch {
+			'U' => new( 0,  1),
+			'D' => new( 0, -1),
+			'R' => new( 1,  0),
+			'L' => new(-1,  0),
+		};
+		for (int knot = 1; knot < 10; knot++) {
+			var vect = rope[knot-1] - rope[knot]; 
+			if (vect.Dist() < 2) break;
+			rope[knot] += vect.Uno();
+		}
+		beenThereDoneThat.Add(rope[9]); // Add the final one to the list
+	}
+}
+Console.WriteLine(beenThereDoneThat.Count);
+
+public record Posi(int PosX = 0, int PosY = 0)
+{
+	public static Posi operator +(Posi a, Posi b) => new(a.PosX + b.PosX, a.PosY + b.PosY);
+	public static Posi operator -(Posi a, Posi b) => new(a.PosX - b.PosX, a.PosY - b.PosY);
+	public int Dist() => Math.Max(Math.Abs(PosX), Math.Abs(PosY));
+	// Max it maximum 1/-1 in both X and
+	public Posi Uno() => new (Math.Sign(PosX) * Math.Min(1, Math.Abs(PosX))
+							, Math.Sign(PosY) * Math.Min(1, Math.Abs(PosY)));
+}

bb9bac6c32d68512ef568b5f171456fc2d59dd15
Hanno Ottens <hpottens@arkey.systems>
2022-12-11 16:18:02 +0100

    Day 10: GPU programming

---
+++ 2022/day_10/a/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] cmnds = File.ReadAllLines("../in.txt");
+
+int varX = 1;
+int wave = 0;
+int clck = 1;
+for (int cmnd = 0; cmnd < cmnds.Length; cmnd++)
+{
+	(int tick, int incr) = cmnds[cmnd][0] switch {
+		'n' => (1, 0),
+		'a' => (2, int.Parse(cmnds[cmnd].Split(' ')[1]))
+	};
+
+	while (tick > 0) {
+		if (clck % 40 == 20 && clck <= 220) {
+			Console.WriteLine($"Clock: {clck}, ValX: {varX}, Value: {clck * varX}");
+			wave += clck * varX;
+		}
+		clck++;
+		tick--;
+	}
+
+	varX += incr;
+}
+Console.WriteLine(wave);
---
+++ 2022/day_10/b/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] cmnds = File.ReadAllLines("../in.txt");
+bool[,] screen = new bool[6, 40];
+
+int varX = 1;
+int clck = 0;
+for (int cmnd = 0; cmnd < cmnds.Length; cmnd++)
+{
+	(int tick, int incr) = cmnds[cmnd][0] switch {
+		'n' => (1, 0),
+		'a' => (2, int.Parse(cmnds[cmnd].Split(' ')[1]))
+	};
+
+	while (tick > 0) {
+		int posY = clck / 40;
+		int posX = clck % 40;
+		screen[posY, posX] = (posX >= varX - 1) && (posX <= varX + 1);
+		clck++;  
+		tick--;
+	}
+
+	varX += incr;
+}
+
+for (int y = 0; y < 6; y++){
+	for (int x = 0; x < 40; x++)
+		Console.Write(screen[y,x] ? '#' : '.');
+	Console.WriteLine();
+}

e36c28fff0988726c48d6e2b714bfe3896a71517
Hanno Ottens <hpottens@arkey.systems>
2022-12-12 09:47:26 +0100

    Day 9: Been there, but now without C# trickery

--- 2022/day_09/b/Program.cs
+++ 2022/day_09/b/Program.cs
@@ -1,32 +1,24 @@
 ﻿(char, int)[] cmnds = File.ReadAllLines("../in.txt").Select(t => (t[0], int.Parse(t[2..]))).ToArray();
 
-HashSet<Posi> beenThereDoneThat = new();
-Posi[] rope = new Posi[10] { new(), new(), new(), new(), new(), new(), new(), new(), new(), new()};
+HashSet<(int x, int y)> beenThereDoneThat = new();
+(int x, int y)[] rope = Enumerable.Range(0, 10).Select(_ => (0,0)).ToArray();
 beenThereDoneThat.Add(rope[9]);
 foreach ((char dir, int length) in cmnds) {
 	for(int i = 0; i < length; i++)
 	{
-		rope[0] += dir switch {
-			'U' => new( 0,  1),
-			'D' => new( 0, -1),
-			'R' => new( 1,  0),
-			'L' => new(-1,  0),
+		rope[0] = dir switch {
+			'U' => (rope[0].x    , rope[0].y + 1),
+			'D' => (rope[0].x    , rope[0].y - 1),
+			'R' => (rope[0].x + 1, rope[0].y    ),
+			'L' => (rope[0].x - 1, rope[0].y    ),
 		};
 		for (int knot = 1; knot < 10; knot++) {
-			var vect = rope[knot-1] - rope[knot]; 
-			if (vect.Dist() < 2) break;
-			rope[knot] += vect.Uno();
+			(int x, int y) vect = (rope[knot-1].x - rope[knot].x
+								 , rope[knot-1].y - rope[knot].y); 
+			if (Math.Max(Math.Abs(vect.x), Math.Abs(vect.y)) < 2) break;
+			rope[knot] = (rope[knot].x + Math.Sign(vect.x), rope[knot].y + Math.Sign(vect.y));
 		}
 		beenThereDoneThat.Add(rope[9]); // Add the final one to the list
 	}
 }
 Console.WriteLine(beenThereDoneThat.Count);
-
-public record Posi(int PosX = 0, int PosY = 0)
-{
-	public static Posi operator +(Posi a, Posi b) => new(a.PosX + b.PosX, a.PosY + b.PosY);
-	public static Posi operator -(Posi a, Posi b) => new(a.PosX - b.PosX, a.PosY - b.PosY);
-	public int Dist() => Math.Max(Math.Abs(PosX), Math.Abs(PosY));
-	// Max it maximum 1/-1 in both X and
-	public Posi Uno() => new (Math.Sign(PosX), Math.Sign(PosY));
-}

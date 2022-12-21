70548edab425d651048e9a5ac9d9627400e0008e
Hanno Ottens <hpottens@arkey.systems>
2022-12-21 08:11:08 +0100

    Day 21: Lost in translation

---
+++ 2022/day_21/a/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Math;
+Dictionary<string, long> memory = new();
+
+(Func<long>,string[]) getCompute(string formula) {
+	if (char.IsDigit(formula[0])) 
+		return (() => (long.Parse(formula)), new string[0]);
+	else {
+		string[] parts = formula[5] switch {
+			'*' => formula.Split(" * "),
+			'/' => formula.Split(" / "),
+			'+' => formula.Split(" + "),
+			'-' => formula.Split(" - "),
+		};
+		Func<long> compute = formula[5] switch {
+			'*' => () => memory[parts[0]] * memory[parts[1]],
+			'/' => () => memory[parts[0]] / memory[parts[1]],
+			'+' => () => memory[parts[0]] + memory[parts[1]],
+			'-' => () => memory[parts[0]] - memory[parts[1]],
+		};
+		return (compute, parts);
+	}
+}
+
+Monkey[] monkeys = File.ReadAllLines("../in.txt")
+	.Select(x => x.Split(": "))
+	.Select(l => {
+		(Func<long> compute, string[] deps) = getCompute(l[1]);
+		return new Monkey(l[0], compute, deps.ToList());
+	}).ToArray();
+
+
+while(!memory.ContainsKey("root")) {
+	for (int m = 0; m < monkeys.Length; m++) {
+		if (memory.ContainsKey(monkeys[m].name)) continue;
+		if (monkeys[m].dependsOn.Select(x => memory.ContainsKey(x)).All(x => x)) {
+			memory[monkeys[m].name] = monkeys[m].compute();
+		}
+	}
+}
+Console.WriteLine(memory["root"]);
+
+record Monkey(string name, Func<long> compute, List<string> dependsOn);
---
+++ 2022/day_21/b/Program.cs
@@ -0,0 +1,65 @@
+﻿using static System.Math;
+Dictionary<string, long> memory = new();
+Dictionary<string, Monkey> forest = new();
+
+(Func<long>,string[]) getCompute(string formula) {
+	if (char.IsDigit(formula[0])) 
+		return (() => (long.Parse(formula)), new string[0]);
+	else {
+		string[] parts = formula[5] switch {
+			'*' => formula.Split(" * "),
+			'/' => formula.Split(" / "),
+			'+' => formula.Split(" + "),
+			'-' => formula.Split(" - "),
+		};
+		Func<long> compute = formula[5] switch {
+			'*' => () => memory[parts[0]] * memory[parts[1]],
+			'/' => () => memory[parts[0]] / memory[parts[1]],
+			'+' => () => memory[parts[0]] + memory[parts[1]],
+			'-' => () => memory[parts[0]] - memory[parts[1]],
+		};
+		return (compute, parts);
+	}
+}
+
+Monkey[] monkeys = File.ReadAllLines("../in.txt")
+	.Select(x => x.Split(": "))
+	.Select(l => {
+		(Func<long> compute, string[] deps) = getCompute(l[1]);
+		var m = new Monkey(l[0], compute, deps.ToList());
+		forest[m.name] = m;
+		return m;
+	}).ToArray();
+
+// Binary search naar het juiste getal
+long minV = 0;
+long maxV = long.MaxValue/1000;
+memory["qmfl"] = 1;
+memory["qdpj"] = 0;
+while (memory["qmfl"] != memory["qdpj"]) {
+	memory = new();
+	// Pak curr in het midden van min/max
+	long curr = (minV + maxV) / 2;
+	Console.WriteLine($"Curr: {curr}, Min: {minV}, Max: {maxV}");
+	memory["humn"] = curr;
+	// Doorgaan tot de root berekend is
+	while(!memory.ContainsKey("root")) {
+		for (int m = 0; m < monkeys.Length; m++) {
+			// Nog niet berekend en alle dependencies beschibaar?
+			if (!memory.ContainsKey(monkeys[m].name)
+				&& monkeys[m].dependsOn.Select(x => memory.ContainsKey(x)).All(x => x)) 
+			{
+				memory[monkeys[m].name] = monkeys[m].compute();
+			}
+		}
+	}
+	// We weten dat `humn` alleen `qmfl` beinvloed en dat deze groter wordt
+	// als `humn` kleiner wordt. Pas MaxV en MinV aan op basis van dat idee.
+	if (memory["qmfl"] < memory["qdpj"])
+		maxV = curr;
+	else
+		minV = curr;
+}
+Console.WriteLine(memory["humn"]); // THIS ONE!
+
+record Monkey(string name, Func<long> compute, List<string> dependsOn);
